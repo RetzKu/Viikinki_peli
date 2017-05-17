908d0e16736d572317329ec8a2a3b6f444d7eac1
Eetu <eetu.keranen@hotmail.com>
2017-05-17 16:52:35 +0300

    initial

---
+++ Assets/Scripts/Tiles/Tile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TileType
+{
+    Empty,
+    Grass,
+    Sea, 
+    Pate
+}
+
+public class Tile
+{
+    public TileType Type;
+    public int X;
+    public int Y;
+
+    public Tile(int x, int y)
+    {
+        Type = TileType.Empty;
+        X = x;
+        Y = y;
+    }
+}
---
+++ Assets/Scripts/Tiles/TileMap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMap : MonoBehaviour
+{
+    public int Width;
+    public int Heigth;
+    public Sprite GrassSprite;
+
+    private Dictionary<Tile, GameObject> _tileGameObjects; // Tällä saatas takas maailmassa oleva GameObject
+    private Tile[,] _tiles;
+
+
+    void Start()
+    {
+        _tileGameObjects = new Dictionary<Tile, GameObject>(Heigth * Width);
+        _tiles = new Tile[Heigth, Width];
+
+        GameObject parent = new GameObject("Tiles");
+
+        for (int x = 0; x < Width; x++)
+        {
+            for (int y = 0; y < Heigth; y++)
+            {
+                _tiles[x, y] = new Tile(x, y);
+
+                GameObject tileObject = new GameObject("(" + x + "," + y + ")");
+                tileObject.transform.parent = parent.transform;
+                tileObject.transform.position = new Vector3(x, y, 0);
+
+                SpriteRenderer spriteRenderer = tileObject.AddComponent<SpriteRenderer>();
+                spriteRenderer.sprite = GrassSprite;
+
+                _tileGameObjects.Add(_tiles[x, y], tileObject);
+            }
+        }
+    }
+
+    void Update()
+    {
+        // Dunno ? 
+    }
+}

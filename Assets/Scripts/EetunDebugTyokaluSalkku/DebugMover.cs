11d577e2423ca4a5cc17e1df9a636e3d3dd80bdb
Eetu <eetu.keranen@hotmail.com>
2017-06-12 21:58:06 +0300

    Perlin Chunked

---
+++ Assets/Scripts/EetunDebugTyokaluSalkku/DebugMover.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class DebugMover : MonoBehaviour
+{
+    public float Speed = 1f;
+    public float DebugViewRange = 16f; // range per suunta 8f
+
+    private Rigidbody2D body;
+    private Vector2 vel;
+
+    private TileMap tilemap;
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(DebugViewRange, DebugViewRange, DebugViewRange));
+    }
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+
+        tilemap = FindObjectOfType<TileMap>();
+    }
+
+	void Update ()
+    {
+        vel = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+        vel *= Speed;
+        body.MovePosition(body.position + vel);
+
+        tilemap.UpdateTilemap(this);
+    }
+
+    void FixedUpdate()
+    {
+    }
+}

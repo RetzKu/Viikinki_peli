72cc94564a36acfe272069ee24592eb62ddfdc77
RetzKu <retzkut@gmail.com>
2017-06-13 20:04:40 +0300

    movement korjattu
    Siirryttiin pois addforcesta ja laitettiin se vektorin lerpillä

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,73 +6,33 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-
-    public float thrust;
-    public Rigidbody2D rb;
     private Vector2 movement;
-    // Use this for initialization
-
-    public int Max_spd;
-    public float Slowdown;
+    public float divider;
+    public Vector2 destination;
+    public float slowdown;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        destination = transform.position;
     }
 
     // Update is called once per frame
 
     void Update()
     {
-        movement = check_axis_speed();
-        rb.AddForce(movement * thrust * Time.deltaTime);
-        drag_check();
     }
 
     void FixedUpdate()
     {
-
+        transform.position = Vector2.Lerp(transform.position, Input_checker(),slowdown * Time.deltaTime);
     }
 
-    Vector2 check_axis_speed()
+    Vector2 Input_checker()
     {
         Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
-
-        if (rb.velocity.x > Max_spd)
-        {
-            movement.x = 0;
-        }
-        if (rb.velocity.x < -Max_spd)
-        {
-            movement.x = 0;
-        }
-        if (rb.velocity.y > Max_spd)
-        {
-            movement.y = 0;
-        }
-        if (rb.velocity.y < -Max_spd)
-        {
-            movement.y = 0;
-        }
-        return movement;
+        movement = movement / divider;
+        destination += movement;
+        return destination;
     }
 
-    void drag_check()
-    {
-        if(rb.velocity.x > Slowdown && rb.velocity.x < -Slowdown || rb.velocity.y > Slowdown && rb.velocity.y < -Slowdown)
-        {
-            rb.drag = 1;
-        }
-        else
-        {
-            if(rb.drag < 30)
-            {
-                rb.drag = rb.drag * 2;
-            }
-            else
-            {
-                rb.drag = 1;
-            }
-        }
-    }
 }

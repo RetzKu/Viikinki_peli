339f566301170fe0c308374d8715cbbeb5beac92
RetzKu <retzkut@gmail.com>
2017-06-16 09:04:46 +0300

    tallennettu osia joita piti yhdistellä prefabbeihin

--- Assets/Scripts/inventory.cs
+++ Assets/Scripts/inventory.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour {
+public class inventory : MonoBehaviour {
 
     public GameObject PlayerGameobject;
     private List<item_script> inventory_data;

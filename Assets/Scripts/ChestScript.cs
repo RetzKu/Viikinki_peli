afdce95e423240ffa2eea93271a2ee6fa16660e6
RetzKu <retzkut@gmail.com>
2017-06-21 09:19:12 +0300

    Aloin tekemään chesti objektien klikkausta

--- Assets/Scripts/ChestScript.cs
+++ Assets/Scripts/ChestScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ChestScript : MonoBehaviour {
 
     private TextMesh ChestText;
+    private Button ChestButton;
 
     private Vector2 ChestLocation;
     private Vector2 PopUp;
@@ -34,15 +35,21 @@ public class ChestScript : MonoBehaviour {
     void OnTriggerExit2D(Collider2D collision)
     {
         Destroy(ChestText);
+        Destroy(ChestButton);
     }
     
     void AddTextObject()
     {
         ChestText = transform.GetChild(0).gameObject.AddComponent<TextMesh>();
         ChestText.text = "Kukkuu"; ChestText.characterSize = 0.1f; ChestText.fontSize = 40;
-        ChestText.gameObject.AddComponent<Button>();
+        ChestButton = ChestText.gameObject.AddComponent<Button>();
+        ChestButton.onClick.AddListener(ButtonFunction);
 
         //Instantiate(ChestPopUp, transform.GetChild(0)).transform.position = ChestParent.transform.position;
         print("luotu");
+    }
+    void ButtonFunction()
+    {
+        Debug.Log("buttonia painettu");
     }
 }

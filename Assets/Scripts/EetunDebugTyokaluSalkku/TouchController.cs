94daec1e08c6021b5cd9242c259633af7248e238
RetzKu <retzkut@gmail.com>
2017-07-07 10:49:48 +0300

    FxSomewhatdonetso

--- Assets/Scripts/EetunDebugTyokaluSalkku/TouchController.cs
+++ Assets/Scripts/EetunDebugTyokaluSalkku/TouchController.cs
@@ -53,7 +53,7 @@ public class TouchController : MonoBehaviour
         if (SlashLineIndex < MaxLineIndices)
         {
             SlashLineIndices[SlashLineIndex] = position;
-            lineRenderer.positionCount = SlashLineIndex + 1;
+            lineRenderer.numPositions = SlashLineIndex + 1;
             lineRenderer.SetPosition(SlashLineIndex, SlashLineIndices[SlashLineIndex]);
 
             SlashLineIndex++;
@@ -67,7 +67,7 @@ public class TouchController : MonoBehaviour
 
     void DrawToMouse(Vector2 mouse)
     {
-        lineRenderer.positionCount = SlashLineIndex + 1;
+        lineRenderer.numPositions = SlashLineIndex + 1;
         lineRenderer.SetPosition(SlashLineIndex, mouse);
     }
 

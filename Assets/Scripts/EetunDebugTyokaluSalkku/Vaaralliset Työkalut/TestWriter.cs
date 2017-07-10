48dbade19833a2e24ba996c6e61813ad73ce7eba
Eetu <eetu.keranen@hotmail.com>
2017-07-10 22:07:13 +0300

    esi työ chunk serializointiin

---
+++ Assets/Scripts/EetunDebugTyokaluSalkku/Vaaralliset Työkalut/TestWriter.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class TestWriter : MonoBehaviour
+{
+
+    private const int FileSize = 10 * 1024 * 1024;
+    void Start()
+    {
+        var path = Path.Combine(Application.persistentDataPath, "bigfile.dat");
+        try
+        {
+            File.WriteAllBytes(path, new byte[FileSize]);
+            TestFileStream(path);
+            TestBinaryReaders(path);
+        }
+        finally
+        {
+            File.Delete(path);
+        }
+
+        Chunk t = new Chunk();
+    }
+
+    void Update()
+    {
+        TileType[,] a = new TileType[3,3];
+        a[1,1] = TileType.Beach;
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Save(a, "terve");
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            Load("terve");
+        }
+    }
+
+    public static void Save(TileType[,] chunk, string name)
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(name + ".sav");
+        bf.Serialize(file, chunk);
+        file.Close();
+
+        Debug.Log("Saved chunk: " + chunk);
+    }
+
+    public static void Load(string chunkName)
+    {
+        if (File.Exists(chunkName + ".sav"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(chunkName + ".sav", FileMode.Open);
+            TileType[,] loaded = (TileType[,])bf.Deserialize(file);
+
+            PrintTile(loaded);
+
+            file.Close();
+
+            Debug.Log("Loaded Chunk: " + chunkName);
+        }
+    }
+
+    public static void PrintTile(TileType[,] map)
+    {
+        string data = null;
+        foreach (var row in map)
+        {
+            data += row.ToString();
+        }
+        Debug.Log(data);
+    }
+
+
+
+    private void TestFileStream(string path)
+    {
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var readBytes = new byte[FileSize];
+            var log = "Read Size,Time\n";
+            foreach (var readSize in new[] { 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096 })
+            {
+                stream.Position = 0;
+                stopwatch.Reset();
+                stopwatch.Start();
+                var offset = 0;
+                do
+                {
+                    offset += stream.Read(readBytes, offset, Math.Min(readSize, FileSize - offset));
+                }
+                while (offset < FileSize);
+                var time = stopwatch.ElapsedMilliseconds;
+                log += readSize + "," + time + "\n";
+            }
+            Debug.Log(log);
+        }
+    }
+
+    private void TestBinaryReaders(string path)
+    {
+        using (var stream = new FileStream(path, FileMode.Open))
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var log = "Reader,Time\n";
+            var numValues = FileSize / sizeof(ushort);
+            var readValues = new ushort[numValues];
+            var reader = new BinaryReader(stream);
+            stopwatch.Reset();
+            stopwatch.Start();
+            for (var i = 0; i < numValues; ++i)
+            {
+                readValues[i] = reader.ReadUInt16();
+            }
+            var time = stopwatch.ElapsedMilliseconds;
+            log += "BinaryReader," + time + "\n";
+
+            stream.Position = 0;
+            var bufferedReader = new BufferedBinaryReader(stream, 4096);
+            stopwatch.Reset();
+            stopwatch.Start();
+            while (bufferedReader.FillBuffer())
+            {
+                var readValsIndex = 0;
+                for (
+                    var numReads = bufferedReader.NumBytesAvailable / sizeof(ushort);
+                    numReads > 0;
+                    --numReads
+                )
+                {
+                    readValues[readValsIndex++] = bufferedReader.ReadUInt16();
+                }
+            }
+            time = stopwatch.ElapsedMilliseconds;
+            log += "BufferedBinaryReader," + time + "\n";
+            Debug.Log(log);
+        }
+    }
+}

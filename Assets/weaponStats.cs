8f3273beecec71173190c514821e17954cef6acb
joonaoikarinen <joonaoikarinen@kamk.fi>
2017-07-13 13:23:27 +0300

    Hitboxit aseista -> efekteille :arrow_heading_up:

--- Assets/weaponStats.cs
+++ Assets/weaponStats.cs
@@ -6,15 +6,16 @@ public abstract class weaponStats : MonoBehaviour
 {
     [Header("Weapon stats")]
 
-    public float maxDistance;
     public float damage = 1.0f;
     public float attackWeight = 10.0f;
-    public Sprite weaponEffect;
-    public bool onRange = false;
-    public Vector3 weaponEffectScale = new Vector3 (1f, 1f, 1f);
 
-    public virtual bool Get()
-    {
-        return false;
-    }
+    [Header("Effect details")]
+
+    public Sprite weaponEffect;
+    [HideInInspector]
+    public bool onRange = false;
+    public float distance;
+    public Vector3 weaponEffectScale = new Vector3 (1f, 1f, 1f);
+    public Vector3 effectOffSet = new Vector3(0f, 0f, 0f);
+
 }

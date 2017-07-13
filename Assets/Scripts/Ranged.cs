6dbd67aab54bc9ba25816a5dfdc4e1fce1d9afca
RetzKu <retzkut@gmail.com>
2017-07-13 15:32:35 +0300

    1

--- Assets/Scripts/Ranged.cs
+++ Assets/Scripts/Ranged.cs
@@ -25,7 +25,8 @@ public class Ranged : weaponStats
 
         if (Hand.name == "u_l_hand") { Weapon.transform.localRotation = Quaternion.Euler(0, 0, -260); Weapon.transform.localPosition = new Vector3(0.032f, 0.019f, 0); }
         if (Hand.name == "d_r_hand") { Weapon.transform.localRotation = Quaternion.Euler(0, 0, -105); }
-        if (Hand.name == "s_l_hand") { Weapon.transform.localRotation = Quaternion.Euler(0, 0, -67); }
+        if (Hand.name == "s_l_hand") { Weapon.transform.localRotation = Quaternion.Euler(0, 0, -67); Weapon.GetComponent<SpriteRenderer>().sortingOrder = 1; }
+        
 
         if (Weapon.transform.localScale.x < 0){ Vector3 Scale = Weapon.transform.localScale; Scale.x *= -1; Weapon.transform.localScale = Scale;}
     }

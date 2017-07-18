1621c7909dccd8a74de403b739d1d3cd6aa66377
PateKAMK <patrik.broms95@gmail.com>
2017-07-18 08:52:12 +0300

    joonalle lisää juttuja

---
+++ Assets/ParticleSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour {
+
+    public GameObject blood;
+
+    public static ParticleSpawner inctance;
+
+    void start()
+    {
+        if(inctance != null)
+        {
+            inctance = this;
+        }
+    }
+    public void SpawnBlood(Vector2 from,Vector2 where)
+    {
+        var newBlood = Instantiate(blood, new Vector2(0, 0), Quaternion.identity);
+        newBlood.GetComponent<destroyMe>().initParticle(from, where);
+    }
+    
+}

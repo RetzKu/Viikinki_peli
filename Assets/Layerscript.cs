1ee5b27709dc042af2f169de469833cda3eff125
Mika Kaunisto <mikakaunisto@kamk.fi>
2017-08-21 09:37:41 +0300

    lyönti androdille

---
+++ Assets/Layerscript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Layerscript : MonoBehaviour {
+
+    
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate()
+    {
+		var Objects = Physics2D.CircleCastAll(transform.position, 5, Vector2.zero, 0f, LayerMask.GetMask("ObjectLayer"));
+        if(Objects.Length != 0)
+        {
+            foreach(RaycastHit2D t in Objects)
+            {
+                if (t.transform.name != "Trunk")
+                {
+
+                    if (t.transform.position.y > transform.position.y)
+                    {
+                        SpriteRenderer Sprites = t.transform.gameObject.GetComponent<SpriteRenderer>();
+                        Sprites.sortingOrder = 0;
+                    }
+                    else
+                    {
+                        SpriteRenderer Sprites = t.transform.gameObject.GetComponent<SpriteRenderer>();
+                        Sprites.sortingOrder = 50;
+                    } 
+                }
+
+            }
+        }
+    }
+}
